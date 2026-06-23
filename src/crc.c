18b1ef9b4cbb5fd6eb3e57630d17f261b890658e
Baris Cakir <8ariscakir@gmail.com>
2026-06-23 14:02:03 +0300

    Add Modbus CRC16 implementation

---
+++ src/crc.c
@@ -0,0 +1,22 @@
+#include "crc.h"
+
+uint16_t modbus_crc16(const uint8_t *data, size_t len)
+{
+    uint16_t crc = 0xFFFF;
+
+    if (data == NULL) {
+        return crc;
+    }
+
+    for (size_t i = 0; i < len; i++) {
+        crc ^= (uint16_t)data[i];
+        for (int bit = 0; bit < 8; bit++) {
+            if (crc & 0x0001) {
+                crc = (uint16_t)((crc >> 1) ^ 0xA001);
+            } else {
+                crc >>= 1;
+            }
+        }
+    }
+    return crc;
+}
